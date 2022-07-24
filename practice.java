445c825833217e9c262d7374a1df071fc19b75a3
Maogasteve <stevemagare4@gmail.com>
2022-07-24 14:34:45 +0300

    first commit

--- practice.java
+++ practice.java
@@ -1,3 +1,12 @@
-String name = "Stephen";
-System.out.println(name
-)
+import javax.swing.*;
+
+public class Messaging {
+    public static void main(String[] args) {
+
+        int num1 = Integer.parseInt(JOptionPane.showInputDialog("Enter number 1"));
+        int num2 = Integer.parseInt(JOptionPane.showInputDialog("Enter the second number"));
+        int product = num1 * num2;
+        JOptionPane.showMessageDialog(null, product);
+
+    }
+}
